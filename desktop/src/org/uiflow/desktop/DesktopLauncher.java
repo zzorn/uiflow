14ac175f9fd238719e2d234daecf9d0bab34b97c
zzorn <zzorn@iki.fi>
2014-11-07 02:01:57 +0200

    Added initial generated libgdx project.

---
+++ desktop/src/org/uiflow/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.uiflow.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.uiflow.UiFlowExample;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UiFlowExample(), config);
+	}
+}
